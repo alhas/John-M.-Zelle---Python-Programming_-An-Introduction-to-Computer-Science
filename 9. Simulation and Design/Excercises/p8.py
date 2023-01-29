9ce3d2629c75d5b654b52f9f4269f3e509d617bd
alhas <bahtiyaralialhas@gmail.com>
2023-01-29 01:27:30 +0100

    problem 8

--- 9. Simulation and Design/Excercises/p8.py
+++ 9. Simulation and Design/Excercises/p8.py
@@ -5,4 +5,63 @@
 # TODO Dealer Function
 # TODO Tournament Function
 # TODO Probability Function
+import random
 
+BLACK_JACK = 21
+SOFT_HAND = 17
+
+def input():
+    inp = int(input("How many times do you want to play?: "))
+    return inp
+
+
+def tournament():
+    pass
+
+
+def player(play):
+
+    hand = 0
+    player_card = draw_card()
+    
+    if hand <= SOFT_HAND and type(player_card[1]) == bool:
+            player_card[1] = True
+
+    while hand <= SOFT_HAND:
+        if player_card[1] == True:
+            hand += 1
+        else: hand += 11
+
+        if player_card[0] in ['J','Q','K']:
+            hand += player_card[1]  
+
+
+
+
+
+def dealer():
+    pass
+
+
+def draw_card():
+    cards = {"J": 10,
+             "Q": 10,
+             "K": 10,
+             "A": bool,
+             "N": (2, 3, 4, 5, 6, 7, 8, 9, 10)
+             }
+
+    a = random.choice(list(cards.items()))
+    if type(a[1]) == tuple:
+        a = random.choice(a[1])
+
+    return a
+
+
+def main():
+
+    pass
+
+
+if __name__ == "__main__":
+    main()
---
+++ 9. Simulation and Design/Excercises/test_p8.py
@@ -0,0 +1,9 @@
+import p8
+
+
+a = p8.draw_card()
+print(a)
+
+
+if hand <= 17 and type(player_card[1]) == bool:
+            player_card[1] = True
